5427a8c272086a00cac67ef0eddaa76b2caee7e5
Joel <jsabonete09@gmail.com>
2026-04-22 14:04:34 +0200

    vite configure

--- portfolio/vite.config.js
+++ portfolio/vite.config.js
@@ -2,8 +2,12 @@
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/',
   plugins: [react()],
   server: {
     port: 3000
+  },
+  build: {
+    outDir: 'dist'
   }
 })
